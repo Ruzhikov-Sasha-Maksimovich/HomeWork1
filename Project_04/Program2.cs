52b30694211ec11f26da68d95201df195e3fe77f
Ruzhikov-Sasha-Maksimovich <109194754+Ruzhikov-Sasha-Maksimovich@users.noreply.github.com>
2022-08-25 14:16:32 +0800

    Update Program2.cs

--- Project_04/Program2.cs
+++ Project_04/Program2.cs
@@ -1,5 +1,48 @@
-﻿System.Console.Clear();
+int[] NewArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        array[i] = new Random().Next(1, 35);
+    return array;
+}
+int PrintFirstArray(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        count = count + array[i];
+        System.Console.Write(count + ", ");
+        count = count - count;
+    }
+    return count;
+}
+void PrintProdaction(int[] array)
+{
+    string printProdaction =" ";
+    if (array.Length > 4)
+    {
+        printProdaction = "изначальный массив -> конечный массив";
+        System.Console.WriteLine(printProdaction);
+    }
 
-System.Console.WriteLine("Введите свое число: ");
-int number = int.Parse(System.Console.ReadLine()!);
-System.Console.WriteLine($"Ваше чиcло {(number % 100) / 10}");
+    else if(array.Length < 5 && array.Length > 3)
+    {
+        printProdaction = "изначальный -> конечный";
+        System.Console.WriteLine(printProdaction);
+    }
+    else if(array.Length < 4 && array.Length > 2)
+    {
+        printProdaction = "начальный -->  конечный";
+        System.Console.WriteLine(printProdaction);
+    }
+    else
+    {
+        System.Console.WriteLine("начало -> конец");
+    }
+}
+int rnd = new Random().Next(2, 6);
+int[] array = new int[rnd];
+NewArray(array);
+PrintProdaction(array);
+PrintFirstArray(array);
+System.Console.Write("-> ");
+System.Console.WriteLine("[" + string.Join(", ", array) + "]");
